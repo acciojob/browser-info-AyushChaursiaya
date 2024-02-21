76413dc28f341d7daef24fbe2b58b3da1c2129cf
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-02-21 13:49:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+	const browserName = navigator.appName;
+	const version = navigator.appVersion;
+
+	const displayContent = document.getElementById("browser-info");
+
+	displayContent.textContent =  "You are using " + browserName + " version " + version;
+})
